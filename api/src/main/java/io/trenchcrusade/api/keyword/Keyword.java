cd52eef3cd02ee89862f24589c10b247eaff6e2c
Steven Justin <sjustin@revealdata.com>
2024-04-12 13:13:03 -0500

    Working Keyword API

--- api/src/main/java/io/trenchcrusade/api/keyword/Keyword.java
+++ api/src/main/java/io/trenchcrusade/api/keyword/Keyword.java
@@ -10,4 +10,28 @@ public class Keyword {
     private String keyword;
     @Column(columnDefinition = "TEXT")
     private String definition;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
 }
--- api/src/main/java/io/trenchcrusade/api/keyword/KeywordController.java
+++ api/src/main/java/io/trenchcrusade/api/keyword/KeywordController.java
@@ -1,9 +1,15 @@
 package io.trenchcrusade.api.keyword;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping(path="/keyword")
 public class KeywordController {
+    @Autowired
     private KeywordRepository keywordRepository;
 
     @GetMapping(path = "/all")
