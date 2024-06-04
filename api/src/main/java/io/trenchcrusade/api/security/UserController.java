3e0f8a569e641ab98b82d1e09561281de55a8433
Steven Justin <sjustin@revealdata.com>
2024-06-04 10:38:54 -0500

    Messing with CrossOrigin again

--- api/src/main/java/io/trenchcrusade/api/security/UserController.java
+++ api/src/main/java/io/trenchcrusade/api/security/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin("http://www.trenchcrusade.builders")
 @RequestMapping(path="/user")
 public class UserController {
     @Autowired
