e6ab2e165aaa79ae935a8bc7d56b9864a31f1ba6
Steven Justin <sjustin@revealdata.com>
2024-07-11 10:00:01 -0500

    Simplifying Roster page

--- frontend/src/routes/builder/warband/_warbandAssets.jsx
+++ frontend/src/routes/builder/warband/_warbandAssets.jsx
@@ -11,17 +11,17 @@ export default function WarbandAssets({warband, warbandEquipment}) {
     return(
         <>
             <div className='row justify-content-center'>
-                <div className='col-sm-12 col-md-4 mb-3'>
+                <div className='col-6 col-md-4 mb-3'>
                     <div className='input-group'>
-                        <span className='input-group-text font-english-towne' id='basic-addon1'>Pay Chest</span>
+                        <span className='input-group-text font-english-towne' id='basic-addon1'>Ducats</span>
                         <input type='number' className='form-control' placeholder='Ducats' aria-label='Ducats' aria-describedby='basic-addon2'
                             id='ducats' value={warband.ducats} onInput={(event) => dispatch(updateWarband(warband, event))}/>
                     </div>
                 </div>
 
-                <div className='col-sm-12 col-md-4 mb-3'>
+                <div className='col-6 col-md-4 mb-3'>
                     <div className='input-group'>
-                        <span className='input-group-text font-english-towne' id='basic-addon1'>Glory Points</span>
+                        <span className='input-group-text font-english-towne' id='basic-addon1'>Glory</span>
                         <input type='number' className='form-control' placeholder='Glory' aria-label='Glory' aria-describedby='basic-addon2'
                             id='glory' value={warband.glory} onInput={(event) => dispatch(updateWarband(warband, event))}/>
                     </div>
