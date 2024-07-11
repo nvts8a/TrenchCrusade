caee54fb09f24c61497895946186bf5a6384f35c
Steven Justin <sjustin@revealdata.com>
2024-07-11 10:01:06 -0500

    Removing Chronology for now

--- frontend/src/routes/builder/warband/_warbandDetails.jsx
+++ frontend/src/routes/builder/warband/_warbandDetails.jsx
@@ -15,16 +15,6 @@ export default function WarbandDetails({warband}) {
                     </div>
                 </div>
             </div>
-
-            <div className='row justify-content-center'>
-                <div className='col-sm-12 col-md-10 mb-3'>
-                    <div className='input-group'>
-                        <span className='input-group-text font-english-towne' id='basic-addon1'>Chronology</span>
-                        <textarea type='text' className='form-control' placeholder='Written by a highly unreliable narrator' aria-label='Chronology' aria-describedby='basic-addon2'
-                            id='chronology' defaultValue={warband.chronology} onInput={(event) => dispatch(updateWarband(warband, event))}/>
-                    </div>
-                </div>
-            </div>
         </>
     )
 }
