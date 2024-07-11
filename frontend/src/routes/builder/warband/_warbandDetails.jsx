370d9d6a59f6e453f18e041c6285a1e979fb510a
Steven Justin <sjustin@revealdata.com>
2024-07-11 10:01:41 -0500

    Text reduction

--- frontend/src/routes/builder/warband/_warbandDetails.jsx
+++ frontend/src/routes/builder/warband/_warbandDetails.jsx
@@ -9,7 +9,7 @@ export default function WarbandDetails({warband}) {
             <div className='row justify-content-center'>
                 <div className='col-sm-12 col-md-5 mb-3'>
                     <div className='input-group'>
-                        <span className='input-group-text font-english-towne' id='basic-addon1'>Warband Name</span>
+                        <span className='input-group-text font-english-towne' id='basic-addon1'>Warband</span>
                         <input type='text' className='form-control' placeholder='Warband Name' aria-label='Warband Name' aria-describedby='basic-addon2'
                             id='name' defaultValue={warband.name} onInput={(event) => dispatch(updateWarband(warband, event))}/>
                     </div>
